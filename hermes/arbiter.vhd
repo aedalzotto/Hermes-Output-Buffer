5c41ee2ae85d231f32c1eb321bdef6ffcdf790f1
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2020-04-23 21:35:06 -0300

    Add arbiter

---
+++ hermes/arbiter.vhd
@@ -0,0 +1,152 @@
+--
+--! @file arbiter.vhd
+--! @brief Hermes NoC output arbiter block for output buffers
+--! @details .
+--! @author Angelo Elias Dalzotto (angelo.dalzotto@edu.pucrs.br)
+--! @author Nicolas Lodea (nicolas.lodea@edu.pucrs.br)
+--! @date 2020/04
+-- 
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+use work.HermesPackage.all;
+
+entity arbiter is
+	port(
+		clock:	in	std_logic;
+		reset:	in	std_logic;
+
+		-- Next node interface
+		data_out:	out	regflit;
+		tx:			out	std_logic;
+		credit_i:	in	std_logic;
+
+		-- Buffers interface
+		data_in:	in	arrayNport_regflit;
+		data_av:	in	regNport;
+		data_ack:	out	regNport
+	);
+end entity;
+
+architecture rtl of arbiter is
+	type state is (S_INIT, S_SENDHEADER, S_PACKETSIZE, S_ACKPKT, S_PAYLOAD);
+	signal	active_state:	state;
+
+	signal last:	integer;
+	signal target:	integer;
+	signal target_set:	std_logic;
+
+	signal flit_counter:	regflit;
+begin
+
+	data_out <= (others => '0') when target_set = '0' else
+				data_in(EAST) when target = EAST else
+				data_in(WEST) when target = WEST else
+				data_in(NORTH) when target = NORTH else
+				data_in(SOUTH) when target = SOUTH else
+				data_in(LOCAL);
+
+	tx <= 	'0' when target_set = '0' else
+			data_av(EAST) when target = EAST else
+			data_av(WEST) when target = WEST else
+			data_av(NORTH) when target = NORTH else
+			data_av(SOUTH) when target = SOUTH else
+			data_av(LOCAL);
+
+	data_ack(EAST) <= credit_i when target_set = '1' and target = EAST else '0';
+	data_ack(WEST) <= credit_i when target_set = '1' and target = WEST else '0';
+	data_ack(NORTH) <= credit_i when target_set = '1' and target = NORTH else '0';
+	data_ack(SOUTH) <= credit_i when target_set = '1' and target = SOUTH else '0';
+	data_ack(LOCAL) <= credit_i when target_set = '1' and target = LOCAL else '0';
+
+	-- Round-robin process
+	process(reset, target_set)
+	begin
+		if reset = '1' then
+			last <= EAST;
+		elsif rising_edge(target_set) then
+			case last is
+				when EAST =>
+					if data_av(WEST) = '1' then target <= WEST;
+					elsif data_av(NORTH) = '1' then target <= NORTH;
+					elsif data_av(SOUTH) = '1' then target <= SOUTH;
+					elsif data_av(LOCAL) = '1' then target <= LOCAL;
+					else target <= EAST;
+					end if;
+				when WEST =>
+					if data_av(NORTH) = '1' then target <= NORTH;
+					elsif data_av(SOUTH) = '1' then target <= SOUTH;
+					elsif data_av(LOCAL) = '1' then target <= LOCAL;
+					elsif data_av(EAST) = '1' then target <= EAST;
+					else target <= WEST;
+					end if;
+				when NORTH =>
+					if data_av(SOUTH) = '1' then target <= SOUTH;
+					elsif data_av(LOCAL) = '1' then target <= LOCAL;
+					elsif data_av(EAST) = '1' then target <= EAST;
+					elsif data_av(WEST) = '1' then target <= WEST;
+					else target <= NORTH;
+					end if;
+				when SOUTH =>
+					if data_av(LOCAL) = '1' then target <= LOCAL;
+					elsif data_av(EAST) = '1' then target <= EAST;
+					elsif data_av(WEST) = '1' then target <= WEST;
+					elsif data_av(NORTH) = '1' then target <= NORTH;
+					else target <= SOUTH;
+					end if;
+				when others =>
+					if data_av(EAST) = '1' then target <= EAST;
+					elsif data_av(WEST) = '1' then target <= WEST;
+					elsif data_av(NORTH) = '1' then target <= NORTH;
+					elsif data_av(SOUTH) = '1' then target <= SOUTH;
+					else target <= LOCAL;
+					end if;
+			end case;
+			last <= target;
+		end if;
+	end process;
+
+	process(reset, clock)
+	begin
+		if reset = '1' then
+			-- East starts with lowest priority
+			target_set <= '0';
+			active_state <= S_INIT;
+		elsif rising_edge(clock) then
+			case active_state is
+				when S_INIT =>
+					-- Check if any buffer has data
+					if	data_av(EAST) = '1' or data_av(WEST) = '1' or 
+							data_av(NORTH) = '1' or	data_av(SOUTH) = '1' or 
+													data_av(LOCAL) = '1' then
+						target_set <= '1';
+						active_state <= S_SENDHEADER;
+					end if;
+
+				when S_SENDHEADER =>
+					-- Wait for first flit transmission
+					if credit_i = '1' then
+						active_state <= S_PACKETSIZE;
+					end if;
+
+				when S_PACKETSIZE =>
+					if data_av(target) = '1' then
+						flit_counter <= data_in(target);
+						active_state <= S_ACKPKT;
+					end if;
+				when S_ACKPKT =>
+					if credit_i = '1' then
+						active_state <= S_PAYLOAD;
+					end if;
+				when S_PAYLOAD =>
+					if flit_counter = x"0" then
+						target_set <= '0';
+						active_state <= S_INIT;
+					elsif credit_i = '1' then
+						flit_counter <= flit_counter - 1;
+					end if;
+			end case;
+		end if;
+	end process;
+end architecture;
--- hermes/node.vhd
+++ hermes/node.vhd
@@ -10,35 +10,60 @@ entity node is
 		clock:	in	std_logic;
 		reset:	in	std_logic;
 
+		--! Input ports
 		clock_rx:	in	regNport;
 		rx:			in	regNport;
 		data_in:	in	arrayNport_regflit;
 		credit_o:	out	regNport;
 
-		--! @todo These should be connected to arbiter
-		data_out:	out array_buffer;
-		data_ack:	in	port_buffer;
-		data_av:	out	port_buffer
+		--! Output ports
+		data_out:	out arrayNport_regflit;
+		credit_i:	in	regNport;
+		tx:			out	regNport
 	);
 end entity;
 
 architecture rtl of node is
+	type array_buffer is array((NPORT-1) downto 0) of arrayNport_regflit;
+	type port_buffer is array((NPORT-1) downto 0) of regNport;
+
 	signal tx_buffers:	port_buffer;
 	signal credit_buffers: port_buffer;
+
+	signal av_buffer:	port_buffer;
+	signal ack_buffer: port_buffer;
+
+	signal data_buffer:	array_buffer;
+
 begin
+	E_arbiter: entity work.arbiter
+	port map(
+		clock => clock,
+		reset => reset, 
+
+		-- Next node interface
+		data_out => data_out(EAST),
+		tx => tx(EAST),
+		credit_i => credit_i(EAST),
+
+		-- Buffers interface
+		data_in => data_buffer(EAST),
+		data_av => av_buffer(EAST),
+		data_ack => ack_buffer(EAST)
+	);
 	
 	E_router: entity work.router
 	port map(
 		clock => clock,
 		reset => reset,
 
-		-- To/from output port
+		-- Read from output port signals
 		clock_rx => clock,
 		rx => rx(EAST),
 		data_in => data_in(EAST),
 		credit_o => credit_o(EAST),
 
-		-- To/from buffer
+		-- Write to buffer signals
 		credit_i => credit_buffers(EAST),
 		tx => tx_buffers(EAST)
 	);
@@ -53,9 +78,9 @@ begin
 		data_in => data_in(EAST),
 		credit_o => credit_buffers(EAST)(EAST),
 
-		data_out => data_out(EAST)(EAST),
-		data_av => data_av(EAST)(EAST),
-		data_ack => data_ack(EAST)(EAST)
+		data_out => data_buffer(EAST)(EAST),
+		data_av => av_buffer(EAST)(EAST),
+		data_ack => ack_buffer(EAST)(EAST)
 	);
 
 	EW_buffer: entity work.outbuffer
@@ -68,9 +93,9 @@ begin
 		data_in => data_in(EAST),
 		credit_o => credit_buffers(EAST)(WEST),
 
-		data_out => data_out(EAST)(WEST),
-		data_av => data_av(EAST)(WEST),
-		data_ack => data_ack(EAST)(WEST)
+		data_out => data_buffer(WEST)(EAST),
+		data_av => av_buffer(WEST)(EAST),
+		data_ack => ack_buffer(WEST)(EAST)
 	);
 
 	EN_buffer: entity work.outbuffer
@@ -83,9 +108,9 @@ begin
 		data_in => data_in(EAST),
 		credit_o => credit_buffers(EAST)(NORTH),
 
-		data_out => data_out(EAST)(NORTH),
-		data_av => data_av(EAST)(NORTH),
-		data_ack => data_ack(EAST)(NORTH)
+		data_out => data_buffer(NORTH)(EAST),
+		data_av => av_buffer(NORTH)(EAST),
+		data_ack => ack_buffer(NORTH)(EAST)
 	);
 
 	ES_buffer: entity work.outbuffer
@@ -98,9 +123,9 @@ begin
 		data_in => data_in(EAST),
 		credit_o => credit_buffers(EAST)(SOUTH),
 
-		data_out => data_out(EAST)(SOUTH),
-		data_av => data_av(EAST)(SOUTH),
-		data_ack => data_ack(EAST)(SOUTH)
+		data_out => data_buffer(SOUTH)(EAST),
+		data_av => av_buffer(SOUTH)(EAST),
+		data_ack => ack_buffer(SOUTH)(EAST)
 	);
 
 	EL_buffer: entity work.outbuffer
@@ -113,18 +138,31 @@ begin
 		data_in => data_in(EAST),
 		credit_o => credit_buffers(EAST)(LOCAL),
 
-		data_out => data_out(EAST)(LOCAL),
-		data_av => data_av(EAST)(LOCAL),
-		data_ack => data_ack(EAST)(LOCAL)
+		data_out => data_buffer(LOCAL)(EAST),
+		data_av => av_buffer(LOCAL)(EAST),
+		data_ack => ack_buffer(LOCAL)(EAST)
+	);
+
+	W_arbiter: entity work.arbiter
+	port map(
+		clock => clock,
+		reset => reset, 
+
+		-- Next node interface
+		data_out => data_out(WEST),
+		tx => tx(WEST),
+		credit_i => credit_i(WEST),
+
+		-- Buffers interface
+		data_in => data_buffer(WEST),
+		data_av => av_buffer(WEST),
+		data_ack => ack_buffer(WEST)
 	);
 
 	W_router: entity work.router
 	port map(
 		clock => clock,
 		reset => reset,
-		
-		-- Routing control
-		 
 
 		-- To/from output port
 		clock_rx => clock,
@@ -147,9 +185,9 @@ begin
 		data_in => data_in(WEST),
 		credit_o => credit_buffers(WEST)(EAST),
 
-		data_out => data_out(WEST)(EAST),
-		data_av => data_av(WEST)(EAST),
-		data_ack => data_ack(WEST)(EAST)
+		data_out => data_buffer(EAST)(WEST),
+		data_av => av_buffer(EAST)(WEST),
+		data_ack => ack_buffer(EAST)(WEST)
 	);
 
 	WW_buffer: entity work.outbuffer
@@ -162,9 +200,9 @@ begin
 		data_in => data_in(WEST),
 		credit_o => credit_buffers(WEST)(WEST),
 
-		data_out => data_out(WEST)(WEST),
-		data_av => data_av(WEST)(WEST),
-		data_ack => data_ack(WEST)(WEST)
+		data_out => data_buffer(WEST)(WEST),
+		data_av => av_buffer(WEST)(WEST),
+		data_ack => ack_buffer(WEST)(WEST)
 	);
 
 	WN_buffer: entity work.outbuffer
@@ -177,9 +215,9 @@ begin
 		data_in => data_in(WEST),
 		credit_o => credit_buffers(WEST)(NORTH),
 
-		data_out => data_out(WEST)(NORTH),
-		data_av => data_av(WEST)(NORTH),
-		data_ack => data_ack(WEST)(NORTH)
+		data_out => data_buffer(NORTH)(WEST),
+		data_av => av_buffer(NORTH)(WEST),
+		data_ack => ack_buffer(NORTH)(WEST)
 	);
 
 	WS_buffer: entity work.outbuffer
@@ -192,9 +230,9 @@ begin
 		data_in => data_in(WEST),
 		credit_o => credit_buffers(WEST)(SOUTH),
 
-		data_out => data_out(WEST)(SOUTH),
-		data_av => data_av(WEST)(SOUTH),
-		data_ack => data_ack(WEST)(SOUTH)
+		data_out => data_buffer(SOUTH)(WEST),
+		data_av => av_buffer(SOUTH)(WEST),
+		data_ack => ack_buffer(SOUTH)(WEST)
 	);
 
 	WL_buffer: entity work.outbuffer
@@ -207,9 +245,25 @@ begin
 		data_in => data_in(WEST),
 		credit_o => credit_buffers(WEST)(LOCAL),
 
-		data_out => data_out(WEST)(LOCAL),
-		data_av => data_av(WEST)(LOCAL),
-		data_ack => data_ack(WEST)(LOCAL)
+		data_out => data_buffer(LOCAL)(WEST),
+		data_av => av_buffer(LOCAL)(WEST),
+		data_ack => ack_buffer(LOCAL)(WEST)
+	);
+
+	N_arbiter: entity work.arbiter
+	port map(
+		clock => clock,
+		reset => reset, 
+
+		-- Next node interface
+		data_out => data_out(NORTH),
+		tx => tx(NORTH),
+		credit_i => credit_i(NORTH),
+
+		-- Buffers interface
+		data_in => data_buffer(NORTH),
+		data_av => av_buffer(NORTH),
+		data_ack => ack_buffer(NORTH)
 	);
 
 	N_router: entity work.router
@@ -241,9 +295,9 @@ begin
 		data_in => data_in(NORTH),
 		credit_o => credit_buffers(NORTH)(EAST),
 
-		data_out => data_out(NORTH)(EAST),
-		data_av => data_av(NORTH)(EAST),
-		data_ack => data_ack(NORTH)(EAST)
+		data_out => data_buffer(EAST)(NORTH),
+		data_av => av_buffer(EAST)(NORTH),
+		data_ack => ack_buffer(EAST)(NORTH)
 	);
 
 	NW_buffer: entity work.outbuffer
@@ -256,9 +310,9 @@ begin
 		data_in => data_in(NORTH),
 		credit_o => credit_buffers(NORTH)(WEST),
 
-		data_out => data_out(NORTH)(WEST),
-		data_av => data_av(NORTH)(WEST),
-		data_ack => data_ack(NORTH)(WEST)
+		data_out => data_buffer(WEST)(NORTH),
+		data_av => av_buffer(WEST)(NORTH),
+		data_ack => ack_buffer(WEST)(NORTH)
 	);
 
 	NN_buffer: entity work.outbuffer
@@ -271,9 +325,9 @@ begin
 		data_in => data_in(NORTH),
 		credit_o => credit_buffers(NORTH)(NORTH),
 
-		data_out => data_out(NORTH)(NORTH),
-		data_av => data_av(NORTH)(NORTH),
-		data_ack => data_ack(NORTH)(NORTH)
+		data_out => data_buffer(NORTH)(NORTH),
+		data_av => av_buffer(NORTH)(NORTH),
+		data_ack => ack_buffer(NORTH)(NORTH)
 	);
 
 	NS_buffer: entity work.outbuffer
@@ -286,9 +340,9 @@ begin
 		data_in => data_in(NORTH),
 		credit_o => credit_buffers(NORTH)(SOUTH),
 
-		data_out => data_out(NORTH)(SOUTH),
-		data_av => data_av(NORTH)(SOUTH),
-		data_ack => data_ack(NORTH)(SOUTH)
+		data_out => data_buffer(SOUTH)(NORTH),
+		data_av => av_buffer(SOUTH)(NORTH),
+		data_ack => ack_buffer(SOUTH)(NORTH)
 	);
 
 	NL_buffer: entity work.outbuffer
@@ -301,9 +355,25 @@ begin
 		data_in => data_in(NORTH),
 		credit_o => credit_buffers(NORTH)(LOCAL),
 
-		data_out => data_out(NORTH)(LOCAL),
-		data_av => data_av(NORTH)(LOCAL),
-		data_ack => data_ack(NORTH)(LOCAL)
+		data_out => data_buffer(LOCAL)(NORTH),
+		data_av => av_buffer(LOCAL)(NORTH),
+		data_ack => ack_buffer(LOCAL)(NORTH)
+	);
+
+	S_arbiter: entity work.arbiter
+	port map(
+		clock => clock,
+		reset => reset, 
+
+		-- Next node interface
+		data_out => data_out(SOUTH),
+		tx => tx(SOUTH),
+		credit_i => credit_i(SOUTH),
+
+		-- Buffers interface
+		data_in => data_buffer(SOUTH),
+		data_av => av_buffer(SOUTH),
+		data_ack => ack_buffer(SOUTH)
 	);
 
 	S_router: entity work.router
@@ -335,9 +405,9 @@ begin
 		data_in => data_in(SOUTH),
 		credit_o => credit_buffers(SOUTH)(EAST),
 
-		data_out => data_out(SOUTH)(EAST),
-		data_av => data_av(SOUTH)(EAST),
-		data_ack => data_ack(SOUTH)(EAST)
+		data_out => data_buffer(EAST)(SOUTH),
+		data_av => av_buffer(EAST)(SOUTH),
+		data_ack => ack_buffer(EAST)(SOUTH)
 	);
 
 	SW_buffer: entity work.outbuffer
@@ -350,9 +420,9 @@ begin
 		data_in => data_in(SOUTH),
 		credit_o => credit_buffers(SOUTH)(WEST),
 
-		data_out => data_out(SOUTH)(WEST),
-		data_av => data_av(SOUTH)(WEST),
-		data_ack => data_ack(SOUTH)(WEST)
+		data_out => data_buffer(WEST)(SOUTH),
+		data_av => av_buffer(WEST)(SOUTH),
+		data_ack => ack_buffer(WEST)(SOUTH)
 	);
 
 	SN_buffer: entity work.outbuffer
@@ -365,9 +435,9 @@ begin
 		data_in => data_in(SOUTH),
 		credit_o => credit_buffers(SOUTH)(NORTH),
 
-		data_out => data_out(SOUTH)(NORTH),
-		data_av => data_av(SOUTH)(NORTH),
-		data_ack => data_ack(SOUTH)(NORTH)
+		data_out => data_buffer(NORTH)(SOUTH),
+		data_av => av_buffer(NORTH)(SOUTH),
+		data_ack => ack_buffer(NORTH)(SOUTH)
 	);
 
 	SS_buffer: entity work.outbuffer
@@ -380,9 +450,9 @@ begin
 		data_in => data_in(SOUTH),
 		credit_o => credit_buffers(SOUTH)(SOUTH),
 
-		data_out => data_out(SOUTH)(SOUTH),
-		data_av => data_av(SOUTH)(SOUTH),
-		data_ack => data_ack(SOUTH)(SOUTH)
+		data_out => data_buffer(SOUTH)(SOUTH),
+		data_av => av_buffer(SOUTH)(SOUTH),
+		data_ack => ack_buffer(SOUTH)(SOUTH)
 	);
 
 	SL_buffer: entity work.outbuffer
@@ -395,9 +465,25 @@ begin
 		data_in => data_in(SOUTH),
 		credit_o => credit_buffers(SOUTH)(LOCAL),
 
-		data_out => data_out(SOUTH)(LOCAL),
-		data_av => data_av(SOUTH)(LOCAL),
-		data_ack => data_ack(SOUTH)(LOCAL)
+		data_out => data_buffer(LOCAL)(SOUTH),
+		data_av => av_buffer(LOCAL)(SOUTH),
+		data_ack => ack_buffer(LOCAL)(SOUTH)
+	);
+
+	L_arbiter: entity work.arbiter
+	port map(
+		clock => clock,
+		reset => reset, 
+
+		-- Next node interface
+		data_out => data_out(LOCAL),
+		tx => tx(LOCAL),
+		credit_i => credit_i(LOCAL),
+
+		-- Buffers interface
+		data_in => data_buffer(LOCAL),
+		data_av => av_buffer(LOCAL),
+		data_ack => ack_buffer(LOCAL)
 	);
 
 	L_router: entity work.router
@@ -429,9 +515,9 @@ begin
 		data_in => data_in(LOCAL),
 		credit_o => credit_buffers(LOCAL)(EAST),
 
-		data_out => data_out(LOCAL)(EAST),
-		data_av => data_av(LOCAL)(EAST),
-		data_ack => data_ack(LOCAL)(EAST)
+		data_out => data_buffer(EAST)(LOCAL),
+		data_av => av_buffer(EAST)(LOCAL),
+		data_ack => ack_buffer(EAST)(LOCAL)
 	);
 
 	LW_buffer: entity work.outbuffer
@@ -444,9 +530,9 @@ begin
 		data_in => data_in(LOCAL),
 		credit_o => credit_buffers(LOCAL)(WEST),
 
-		data_out => data_out(LOCAL)(WEST),
-		data_av => data_av(LOCAL)(WEST),
-		data_ack => data_ack(LOCAL)(WEST)
+		data_out => data_buffer(WEST)(LOCAL),
+		data_av => av_buffer(WEST)(LOCAL),
+		data_ack => ack_buffer(WEST)(LOCAL)
 	);
 
 	LN_buffer: entity work.outbuffer
@@ -459,9 +545,9 @@ begin
 		data_in => data_in(LOCAL),
 		credit_o => credit_buffers(LOCAL)(NORTH),
 
-		data_out => data_out(LOCAL)(NORTH),
-		data_av => data_av(LOCAL)(NORTH),
-		data_ack => data_ack(LOCAL)(NORTH)
+		data_out => data_buffer(NORTH)(LOCAL),
+		data_av => av_buffer(NORTH)(LOCAL),
+		data_ack => ack_buffer(NORTH)(LOCAL)
 	);
 
 	LS_buffer: entity work.outbuffer
@@ -474,9 +560,9 @@ begin
 		data_in => data_in(LOCAL),
 		credit_o => credit_buffers(LOCAL)(SOUTH),
 
-		data_out => data_out(LOCAL)(SOUTH),
-		data_av => data_av(LOCAL)(SOUTH),
-		data_ack => data_ack(LOCAL)(SOUTH)
+		data_out => data_buffer(SOUTH)(LOCAL),
+		data_av => av_buffer(SOUTH)(LOCAL),
+		data_ack => ack_buffer(SOUTH)(LOCAL)
 	);
 
 	LL_buffer: entity work.outbuffer
@@ -489,9 +575,9 @@ begin
 		data_in => data_in(LOCAL),
 		credit_o => credit_buffers(LOCAL)(LOCAL),
 
-		data_out => data_out(LOCAL)(LOCAL),
-		data_av => data_av(LOCAL)(LOCAL),
-		data_ack => data_ack(LOCAL)(LOCAL)
+		data_out => data_buffer(LOCAL)(LOCAL),
+		data_av => av_buffer(LOCAL)(LOCAL),
+		data_ack => ack_buffer(LOCAL)(LOCAL)
 	);
 
 end architecture;
